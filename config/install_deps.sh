055e0af1cbf514c43015b58e6442246898dddf4e
smk762 <smk762@iinet.net.au>
2019-07-08 00:29:39 -0400

    3rd party

---
+++ config/install_deps.sh
@@ -0,0 +1,4 @@
+sudo apt-get install python3-dev python3 libgnutls28-dev libssl-dev python3-pip -y
+pip3 install setuptools --user
+pip3 install wheel --user
+pip3 install base58 slick-bitcoinrpc requests python-bitcoinlib configparser --user
