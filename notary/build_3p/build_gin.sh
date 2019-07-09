a8377d643dd1df5d139bfc62bd74e6c0e75811fd
smk762 <smk762@iinet.net.au>
2019-07-09 10:04:09 -0400

    if you build it, the ntx will come

---
+++ notary/build_3p/build_gin.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+cd ~
+git clone https://github.com/GIN-coin/gincoin-core
+cd gincoin-core
+git checkout master
+
+berkeleydb () {
+    GIN_ROOT="~/gincoin-core"
+    GIN_PREFIX="${GIN_ROOT}/db4"
+    mkdir -p $GIN_PREFIX
+    wget -N 'http://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz'
+    echo '12edc0df75bf9abd7f82f821795bcee50f42cb2e5f76a6a281b85732798364ef db-4.8.30.NC.tar.gz' | sha256sum -c
+    tar -xzvf db-4.8.30.NC.tar.gz
+    cd db-4.8.30.NC/build_unix/
+
+    ../dist/configure -enable-cxx -disable-shared -with-pic -prefix=$GIN_PREFIX
+
+    make install
+    cd $GIN_ROOT
+}
+
+buildgin () {
+    git pull
+    make clean
+    ./autogen.sh
+    ./configure LDFLAGS="-L${GIN_PREFIX}/lib/" CPPFLAGS="-I${GIN_PREFIX}/include/" --with-gui=no --disable-tests --disable-bench --without-miniupnpc --enable-experimental-asm --enable-static --disable-shared --without-gui
+    make -j$(nproc)
+}
+
+berkeleydb
+buildgin
+
