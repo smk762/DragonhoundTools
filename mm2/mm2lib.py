5bf617978c2233c7c5d62fd768d4b0a1656178e4
smk762 <smk762@iinet.net.au>
2019-07-10 09:23:18 -0400

    mm2

--- mm2/mm2lib.py
+++ mm2/mm2lib.py
@@ -4,7 +4,172 @@ import requests
 from pprint import pprint
 from mm2lib import *
 
+source_node_ip="127.0.0.1"
 node_ip = "http://"+source_node_ip+":7783"
+
+def get_trade_fee(coin=''):
+    if coin in coins:
+        params = {'userpass': user_pass,
+                  'method': 'get_trade_fee',
+                  'coin': cointag,}
+        r = requests.post(node_ip, json=params)
+        return r
+    else:
+        print("Coin ["+coin+"] not recognised! Is it enabled?")
+
+def my_balance(coin=''):
+    if coin in coins:
+        params = {'userpass': user_pass,
+                  'method': 'my_balance',
+                  'coin': cointag,}
+        r = requests.post(node_ip, json=params)
+        return r
+    else:
+        print("Coin ["+coin+"] not recognised! Is it enabled?")
+
+def buy(base='', rel='', price=0, volume=0):
+    if 'base' != '':
+        if 'base' not in coins:
+            print("base coin not recognised.")
+            sys.exit(0)
+        if 'rel' in coins:
+            print("rel coin not recognised.")
+            sys.exit(0)
+        if float(price) >= 0:
+            balance = my_balance(base)
+            fee = get_trade_fee(base)*2
+            available = balance - fee
+            if volume > available:
+                print("Volume too high!")
+                print("Balance: "+str(balance))
+                print("Fee: "+str(balance))
+                print("Available: "+str(balance))
+                sys.exit(0)
+            else:
+                params = {'userpass': userpass,
+                          'method': 'buy',
+                          'base': base,
+                          'rel': rel,
+                          'price': price,
+                          'volume': volume,
+                          }
+                r = requests.post(node_ip,json=params)
+                return r
+        else:
+            print("Price must be above 0")
+            sys.exit(0)
+    else:
+        print("Use like ./atomicdex-cli buy BASE REL PRICE VOLUME")
+        sys.exit(0)
+
+
+def sell(base='', rel='', price=0, volume=0):
+    if 'base' != '':
+        if 'base' not in coins:
+            print("base coin not recognised.")
+            sys.exit(0)
+        if 'rel' in coins:
+            print("rel coin not recognised.")
+            sys.exit(0)
+        if float(price) >= 0:
+            balance = my_balance(base)
+            fee = get_trade_fee(base)*2
+            available = balance - fee
+            if volume > available:
+                print("Volume too high!")
+                print("Balance: "+str(balance))
+                print("Fee: "+str(balance))
+                print("Available: "+str(balance))
+                sys.exit(0)
+            else:
+                params = {'userpass': userpass,
+                          'method': 'sell',
+                          'base': base,
+                          'rel': rel,
+                          'price': price,
+                          'volume': volume,
+                          }
+                r = requests.post(node_ip,json=params)
+                return r
+        else:
+            print("Price must be above 0")
+            sys.exit(0)
+    else:
+        print("Use like ./atomicdex-cli sell BASE REL PRICE VOLUME")
+        sys.exit(0)
+
+
+"base\":\"BASE\",\"rel\":\"REL\",\"price\":\"0.9\",\"max\":true}
+"base\":\"BASE\",\"rel\":\"REL\",\"price\":\"0.9\",\"volume\":\"1\"}
+
+def setprice(base='', rel='', price=0, volume=0):
+    if 'base' != '':
+        if 'base' not in coins:
+            print("base coin not recognised.")
+            sys.exit(0)
+        if 'rel' in coins:
+            print("rel coin not recognised.")
+            sys.exit(0)
+        if float(price) >= 0:
+            if volume == 'max':
+                params = {'userpass': userpass,
+                          'method': 'setprice',
+                          'base': base,
+                          'rel': rel,
+                          'price': price,
+                          'max': 'true',
+                          }
+                r = requests.post(node_ip,json=params)
+                return r
+            elif float(volume) >= 0:
+                balance = my_balance(base)
+                fee = get_trade_fee(base)*2
+                available = balance - fee
+                if volume > available:
+                    print("Volume too high!")
+                    print("Balance: "+str(balance))
+                    print("Fee: "+str(balance))
+                    print("Available: "+str(balance))
+                    sys.exit(0)
+                else:
+                    params = {'userpass': userpass,
+                              'method': 'setprice',
+                              'base': base,
+                              'rel': rel,
+                              'price': price,
+                              'volume': volume,
+                              }
+                    r = requests.post(node_ip,json=params)
+                    return r
+            else:
+                print("Volume must be above 0")
+                sys.exit(0)
+        else:
+            print("Price must be above 0")
+            sys.exit(0)
+    else:
+        print("Use like ./atomicdex-cli sell BASE REL PRICE VOLUME")
+        sys.exit(0)
+
+
+def cancel_all_orders(base='', rel=''):    
+    cancel_by = {"type":"All"}
+    if 'base' != '':
+        if 'base' in coin:
+            if 'rel' in coins:
+                cancel_by = {"type":"Pair", "data":{"base":base,"rel":rel},}
+            else:
+                print("rel coin not recognised.")
+                sys.exit(0)
+        else:
+            print("base coin not recognised.")
+            sys.exit(0)
+    params = {'userpass': userpass,
+              'method': 'cancel_all_orders',
+              'cancel_by': cancel_by,}
+    r = requests.post(node_ip,json=params)
+    return r
+
 try:
     method = sys.argv[1]
 except:
@@ -23,19 +188,6 @@ cointag_methods = [ 'withdraw', 'my_balance']
 if method in noparam_methods:
     r = mm2_method(node_ip, userpass, method)
 elif method == 'cancel_all_orders':
-    if len(sys.argv) < 3:
-        cancel_by = {"type":"All"}
-    elif len(sys.argv) == 4:
-        base = sys.argv[2]
-        rel = sys.argv[3]
-        cancel_by = {"type":"Pair", "data":{"base":base,"rel":rel},}
-    else:
-        print("Use like ./atomicdex-cli cancel_all_orders BASE REL")
-        sys.exit(0)
-    params = {'userpass': userpass,
-              'method': 'cancel_all_orders',
-              'cancel_by': cancel_by,}
-    r = requests.post(node_ip,json=params)
 elif method == 'withdraw':
     if len(sys.argv) == 5:
         cointag = sys.argv[2]
@@ -85,4 +237,4 @@ else:
 try:
     pprint(r.json(), indent=1)
 except:
-print(r.text)
+    print(r.text)
