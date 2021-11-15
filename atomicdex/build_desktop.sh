6ecd7351e2b60998e3bb1c01f8c2edd387ef9ba2
smk762 <35845239+smk762@users.noreply.github.com>
2021-11-15 18:43:26 +0800

    create desktop build script

---
+++ atomicdex/build_desktop.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Before using this script, you need to make sure the AtomicDEX-Desktop
+# repository has been cloned including the recursive submodules. 
+
+# This script takes an optional parameter to define the branch, otherwise it defaults to dev.
+
+cd ~/GITHUB/KP/atomicDEX-Desktop  # Change this to the path you cloned AtomicDEX into
+if [[ ${#1} -ne 0  ]]; then
+	echo "checking out ${1} branch"
+	git checkout $1
+else 
+	echo "checking out dev branch"
+	git checkout dev
+fi;
+rm -rf build
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release ../ -GNinja
+cmake --build . --config Release --target atomicdex-desktop
+cd bin/AntaraAtomicDexAppDir/usr/bin
+./atomicdex-desktop
