f58a332a3e41b5ebd381d587e7b27e8408454f9a
smk762 <35845239+smk762@users.noreply.github.com>
2022-01-07 13:04:23 +0800

    Create lib_base58.py

---
+++ misc_libs/lib_base58.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+import base58
+import logging
+import logging.handlers
+from logging import Handler, Formatter
+import bitcoin
+from bitcoin.core import x
+from bitcoin.core import CoreMainParams
+from bitcoin.wallet import P2PKHBitcoinAddress
+
+logger = logging.getLogger()
+handler = logging.StreamHandler()
+formatter = Formatter('%(asctime)s %(levelname)-8s %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
+
+
+class KMD_CoinParams(CoreMainParams):
+    MESSAGE_START = b'\x24\xe9\x27\x64'
+    DEFAULT_PORT = 7770
+    BASE58_PREFIXES = {'PUBKEY_ADDR': 60,
+                       'SCRIPT_ADDR': 85,
+                       'SECRET_KEY': 188}
+
+class PBC_CoinParams(CoreMainParams):
+    MESSAGE_START = b'\x24\xe9\x27\x64'
+    DEFAULT_PORT = 7770
+    BASE58_PREFIXES = {'PUBKEY_ADDR': 60,
+                       'SCRIPT_ADDR': 85,
+                       'SECRET_KEY': 188}
+
+class BTC_CoinParams(CoreMainParams):
+    MESSAGE_START = b'\x24\xe9\x27\x64'
+    DEFAULT_PORT = 7770
+    BASE58_PREFIXES = {'PUBKEY_ADDR': 0,
+                       'SCRIPT_ADDR': 5,
+                       'SECRET_KEY': 128}
+
+class LTC_CoinParams(CoreMainParams):
+    MESSAGE_START = b'\x24\xe9\x27\x64'
+    DEFAULT_PORT = 7770
+    BASE58_PREFIXES = {'PUBKEY_ADDR': 48,
+                       'SCRIPT_ADDR': 5,
+                       'SECRET_KEY': 176}
+
+class AYA_CoinParams(CoreMainParams):
+    MESSAGE_START = b'\x24\xe9\x27\x64'
+    DEFAULT_PORT = 7770
+    BASE58_PREFIXES = {'PUBKEY_ADDR': 23,
+                       'SCRIPT_ADDR': 5,
+                       'SECRET_KEY': 176}
+
+class EMC2_CoinParams(CoreMainParams):
+    MESSAGE_START = b'\x24\xe9\x27\x64'
+    DEFAULT_PORT = 7770
+    BASE58_PREFIXES = {'PUBKEY_ADDR': 33,
+                       'SCRIPT_ADDR': 5,
+                       'SECRET_KEY': 176}
+
+class GAME_CoinParams(CoreMainParams):
+    MESSAGE_START = b'\x24\xe9\x27\x64'
+    DEFAULT_PORT = 7770
+    BASE58_PREFIXES = {'PUBKEY_ADDR': 38,
+                       'SCRIPT_ADDR': 5,
+                       'SECRET_KEY': 166}
+
+class GIN_CoinParams(CoreMainParams):
+    MESSAGE_START = b'\x24\xe9\x27\x64'
+    DEFAULT_PORT = 7770
+    BASE58_PREFIXES = {'PUBKEY_ADDR': 38,
+                       'SCRIPT_ADDR': 10,
+                       'SECRET_KEY': 198}
+
+class GLEEC_3P_CoinParams(CoreMainParams):
+    MESSAGE_START = b'\x24\xe9\x27\x64'
+    DEFAULT_PORT = 7770
+    BASE58_PREFIXES = {'PUBKEY_ADDR': 35,
+                       'SCRIPT_ADDR': 38,
+                       'SECRET_KEY': 65}
+
+# Update this if new third party coins added
+coin_params = {
+    "KMD": KMD_CoinParams,
+    "BTC": BTC_CoinParams,
+    "LTC": LTC_CoinParams,
+    "PBC": PBC_CoinParams,
+    "SFUSD": KMD_CoinParams,
+    "AYA": AYA_CoinParams,
+    "EMC2": EMC2_CoinParams,
+    "GAME": GAME_CoinParams,
+    "GIN": GIN_CoinParams,
+    "GLEEC_AC": KMD_CoinParams,
+    "GLEEC_3P": GLEEC_3P_CoinParams
+
+}
+
+def get_addr_from_pubkey(pubkey, coin):
+    bitcoin.params = coin_params[coin]
+    return str(P2PKHBitcoinAddress.from_pubkey(x(pubkey)))
