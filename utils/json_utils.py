5fed97a384e5bd23333e557a845b1b0a7ea1e51d
smk762 <35845239+smk762@users.noreply.github.com>
2022-12-22 21:48:24 +0800

    Update json_utils.py

--- utils/json_utils.py
+++ utils/json_utils.py
@@ -15,12 +15,17 @@ def sort_json_files():
 
 
 def convert_csv(file, has_headers=False):
-    with open(file, mode='r') as csv_file:
-        csv_reader = csv.DictReader(csv_file)
-        line_count = 0
-        data = []
-        for row in csv_reader:
-            data.append(row)
-            line_count += 1
-        print(f'Processed {line_count} lines.')
-    return data
+    for i in os.listdir("."):
+        if i.endswith(".csv"):
+            with open(i, "r") as f:
+                csv_reader = csv.DictReader(f)
+                line_count = 0
+                data = []
+                for row in csv_reader:
+                    data.append(row)
+                    line_count += 1
+                print(f'Processed {line_count} lines.')
+            with open(i, "w") as f:
+                json.dump(data, f, indent=4, sort_keys=True)
+
+
