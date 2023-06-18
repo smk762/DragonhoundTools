2aa29447ab8b228ef23b23f3cb1c7b4589009667
smk762 <35845239+smk762@users.noreply.github.com>
2023-06-18 20:17:10 +0800

    Update seed_checksum.py

--- wallet/seed_checksum.py
+++ wallet/seed_checksum.py
@@ -4,7 +4,8 @@ import mnemonic
 from wordlist import english_wordlist 
  
 m = mnemonic.Mnemonic('english')
+seed_without_checksum_word = input("Enter the first 11 or 23 words: ")
 for word in english:
-    tested = f"{seed} {word}"
+    tested = f"{seed_without_checksum_word} {word}"
     if m.check(tested):
         print(tested)
