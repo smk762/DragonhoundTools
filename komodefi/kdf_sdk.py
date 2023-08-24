97d5b2443f9559dfd30916bb8a34288923a5ae05
smk762 <smk762@iinet.net.au>
2023-08-24 19:20:18 +0800

    add poetry deps

--- komodefi/kdf_sdk.py
+++ komodefi/kdf_sdk.py
@@ -3,18 +3,23 @@ import os
 import sys
 import json
 import requests
+import asyncio
+from websockets.sync.client import connect
+
 from logger import logger
 from os.path import expanduser, dirname, realpath
+
 
 SCRIPT_PATH = dirname(realpath(__file__))
 
 MM2_JSON_PATH = f"{SCRIPT_PATH}/MM2.json"
 
 class KomoDeFi_API():
-    def __init__(self, config: str=MM2_JSON_PATH):
+    def __init__(self, config: str=MM2_JSON_PATH, protocol: str = "http"):
         ip = '127.0.0.1'
         port = 7783
         netid = 7777
+        self.protocol = protocol
         if os.path.isfile(MM2_JSON_PATH):
             with open(MM2_JSON_PATH, "r") as f:
                 conf = json.load(f)
@@ -26,7 +31,7 @@ class KomoDeFi_API():
             if "netid" in conf:
                 self.netid = conf["netid"]
             else: self.netid = 7777
-            self.mm2_ip = f"http://{ip}:{port}"
+            self.mm2_ip = f"{self.protocol}://{ip}:{port}"
         else:
             logger.error(f"Komodefi SDK config not found at {MM2_JSON_PATH}!")
             raise SystemExit(1)
@@ -36,8 +41,9 @@ class KomoDeFi_API():
             raise SystemExit(1)
         # logger.debug("-"*80)
         # logger.info(f"Komodefi SDK version {self.version} is running at {self.mm2_ip} on netID {self.netid}!")
-           
-    def rpc(self, method, params=None, v2=False):
+
+
+    def rpc(self, method, params=None, v2=False, wss=False):
         if not params:
             params = {}
         body = {
@@ -51,7 +57,14 @@ class KomoDeFi_API():
             })
         elif params:
             body.update(params)
-        r = requests.post(self.mm2_ip, json.dumps(body))
+        if wss:
+            with connect(self.mm2_ip) as ws:
+                ws.send(json.dumps(body))
+                data = ws.recv()
+                logger.info(f"Received: {data}")
+                return data
+        else:
+            r = requests.post(self.mm2_ip, json.dumps(body))
         return r
     
     def get_activation_params(self, coin: str, network: str):
@@ -72,4 +85,3 @@ class KomoDeFi_API():
 
 if __name__ == '__main__':
     api = KomoDeFi_API()
-    
